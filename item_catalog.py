adb8ea60133fbc72f9b0bd97fb55489a866c85f8
Cheryl Flowers <cm_flowers@hotmail.com>
2016-03-05 11:22:21 -0500

    feat: Remove hardcoded values and retrieve list of categories from db

--- item_catalog.py
+++ item_catalog.py
@@ -10,13 +10,6 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-# TODO
-# Fake categories to test navigation; delete from live version.
-category = {'name': 'Running', 'id': '1'}
-
-categories = [{'name': 'Running', 'id': '1'},
-              {'name': 'Swimming', 'id': '2'},
-              {'name': 'Volleyball', 'id': '3'}]
 
 # TODO
 # Fake products to test navigation; delete from live version.
@@ -43,62 +36,72 @@ products = [{'name': 'Shoes',
              'id': '3'}]
 
 
+def GetAllCategories():
+    """ Get a list of all categories in the catalog.
+
+    Returns:
+        categories: a list of Category tuples of all categories in the catalog.
+    """
+    categories = session.query(Category).order_by(Category.name)
+    return categories
+
+
 @app.route('/')
 @app.route('/catalog/')
 def categoryListing():
     return render_template('list_all_categories.html',
-                           categories=categories,
+                           categories=GetAllCategories(),
                            products=products)
 
 
 @app.route('/catalog/<int:category_id>/add/')
 def addCategory(category_id):
     return render_template('add_category.html',
-                           categories=categories)
+                           categories=GetAllCategories())
 
 
 @app.route('/catalog/<int:category_id>/edit/')
 def editCategory(category_id):
     return render_template('edit_category.html',
-                           categories=categories)
+                           categories=GetAllCategories())
 
 
 @app.route('/catalog/<int:category_id>/delete/')
 def deleteCategory(category_id):
     return render_template('delete_category.html',
-                           categories=categories)
+                           categories=GetAllCategories())
 
 
 @app.route('/catalog/<int:category_id>/')
 def productListing(category_id):
     return render_template('list_all_products.html',
-                           categories=categories,
+                           categories=GetAllCategories(),
                            products=products)
 
 
 @app.route('/catalog/<int:category_id>/<int:product_id>/view/')
 def viewProduct(category_id, product_id):
     return render_template('view_product.html',
-                           categories=categories,
+                           categories=GetAllCategories(),
                            product=product)
 
 
 @app.route('/catalog/<int:category_id>/<int:product_id>/add/')
 def addProduct(category_id, product_id):
     return render_template('add_product.html',
-                           categories=categories)
+                           categories=GetAllCategories())
 
 
 @app.route('/catalog/<int:category_id>/<int:product_id>/edit/')
 def editProduct(category_id, product_id):
     return render_template('edit_product.html',
-                           categories=categories)
+                           categories=GetAllCategories())
 
 
 @app.route('/catalog/<int:category_id>/<int:product_id>/delete/')
 def deleteProduct(category_id, product_id):
     return render_template('delete_product.html',
-                           categories=categories)
+                           categories=GetAllCategories())
 
 
 if __name__ == '__main__':
